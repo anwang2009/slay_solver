d7383151efaecfb7e3493ffa2310134ced5a86c4
Zoheb0707 <zhs142013@yahoo.in>
2018-12-18 10:28:35 -0800

    added discription ofr attributes needed in cards and potions

--- src/main/java/solver_mod/Solver_Code/Card_Encyclopedia.java
+++ src/main/java/solver_mod/Solver_Code/Card_Encyclopedia.java
@@ -5,6 +5,10 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
 import java.util.Map;
 
+//map c.name --> {attack -> int, block -> int, strength_it_adds -> int, health_it_adds -> int, dexterity_it_adds -> 
+                //vulnerable_it_applies -> int, weak_it_applies -> int, frail_it_applies -> int, 
+                //energy_it_gives -> int, energy_it_takes -> int, target -> String}
+
 public class Card_Encyclopedia {
     // AbstractCard has fields with all necessary info (color, rarity, damage,
     // type, etc.)
--- src/main/java/solver_mod/Solver_Code/Potion_Encyclopedia.java
+++ src/main/java/solver_mod/Solver_Code/Potion_Encyclopedia.java
@@ -5,6 +5,10 @@ import com.megacrit.cardcrawl.potions.*;
 import java.util.HashMap;
 import java.util.Map;
 
+//map p.name --> {attack -> int, block -> int, strength_it_adds -> int, health_it_adds -> int, dexterity_it_adds -> 
+                //vulnerable_it_applies -> int, weak_it_applies -> int, frail_it_applies -> int, 
+                //energy_it_gives -> int, energy_it_takes -> int, target -> String}
+
 public class Potion_Encyclopedia {
     // AbstractPotion has fields for color, rarity, size, and a getPotency()
     // method. These appear to be the only attributes in each potion class
