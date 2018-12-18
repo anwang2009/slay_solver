11db23337a302b8a53e5e41dfea50154a43f215c
Kevin Zhu <kevinzcharlie@gmail.com>
2018-12-18 11:48:35 -0800

    Some minor fixes, documented relevant fields/methods in AbstractCard and AbstractPotion (substitute for the map of maps)

--- src/main/java/solver_mod/Solver_Code/Enemy_Encyclopedia.java
+++ src/main/java/solver_mod/Solver_Code/Enemy_Encyclopedia.java
@@ -1,7 +1,24 @@
 package solver_mod.Solver_Code;
 
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import java.util.HashMap;
 import java.util.Map;
 
+// There's no analogous structure for enemies like the master deck for cards,
+// so we'll just have to update this frequently
 public class Enemy_Encyclopedia {
-    public static Map<String, Map<String, Integer>> dict;
+    // probably have to use reflection or something for AbstractMonster objects,
+    // especially to get important intent info
+    public static Map<String, AbstractMonster> dict = new HashMap<String, AbstractMonster>();
+    static {
+        update();
+    }
+    
+    public static void update() {
+        for (AbstractMonster m : AbstractDungeon.getCurrRoom().monsters.monsters) {
+            dict.put(m.name, m);
+        }
+    }
 }
