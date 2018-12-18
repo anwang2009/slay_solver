56e8f4bacf28f908e6faa3c8778ae37962effe99
Zoheb0707 <zhs142013@yahoo.in>
2018-12-17 18:35:01 -0800

    updated Action.Java

--- src/main/java/solver_mod/Solver_Code/Action.java
+++ src/main/java/solver_mod/Solver_Code/Action.java
@@ -6,6 +6,9 @@ If valid, the new state is calculated and returned.
 */
 
 import java.util.*;
+
+import com.sun.org.apache.xerces.internal.utils.XMLSecurityPropertyManager.State;
+
 import solver_mod.Solver_Code.*;
 
 public class Action {
@@ -15,12 +18,11 @@ public class Action {
     //ppulate the dictionaries
     //in future iterations these will be classes of their own
 
-    public void initScore() {
+    public State newState(State current_state) {
         //what does the user want to do?
         String potion_name = null;
         String card_name = null;
         String target_name = null;
-        State current_state = State.getState();
         
         //1) use a potion
         current_state = use_a_potion(potion_name, target_name, current_state);
@@ -29,7 +31,7 @@ public class Action {
         //3) end turn
         current_state = end_turn(current_state);
         //return current_state
-
+        return current_state;
     }
 
     public State use_a_potion(String potion, String target, State current_state){
@@ -39,19 +41,26 @@ public class Action {
         else if (target.equalsIgnoreCase("self") || target.equalsIgnoreCase("enemy")) {
             //throw exception
         }
-        else {
-            //1) remove potion from state
-            current_state.remove_potion(potion);
-            //2) change player attributes in state
-            //3) change enemy attributes in state
-        }
+        //1) remove potion from state
+        current_state.remove_potion(potion);
+        //2) change player attributes in state
+        //3) change enemy attributes in state
+
+        return current_state;
     }
 
     public State use_a_card(String card, String target, State current_state) {
+        if (!Card_Dict.containsKey(card)) {
+            //throw exception
+        }
         //1) remove card from state
+        current_state.remove_card(card);
         //2) remove the energy the card costs from the state
+        current_state.set_energy(-card.energy);
         //3) change player attributes in state
         //4) change enemy attributes in state
+
+        return current_state;
     }
 
     public State end_turn(State current_state) {
