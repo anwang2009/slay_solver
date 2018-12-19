7781ef12a2166a57b1db39faab01c3de982eb0e2
Zoheb0707 <zhs142013@yahoo.in>
2018-12-19 14:24:33 -0800

    added all cards to dict

--- src/main/java/solver_mod/Solver_Code/Card_Encyclopedia.java
+++ src/main/java/solver_mod/Solver_Code/Card_Encyclopedia.java
@@ -1,9 +1,15 @@
 package solver_mod.Solver_Code;
 
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.colorless.BandageUp;
+import com.megacrit.cardcrawl.cards.colorless.*;
+import com.megacrit.cardcrawl.cards.curses.*;
 import com.megacrit.cardcrawl.cards.green.Backflip;
+import com.megacrit.cardcrawl.cards.green.MasterfulStab;
 import com.megacrit.cardcrawl.cards.red.*;
+import com.megacrit.cardcrawl.cards.status.Burn;
+import com.megacrit.cardcrawl.cards.status.Dazed;
+import com.megacrit.cardcrawl.cards.status.Slimed;
+import com.megacrit.cardcrawl.cards.status.Wound;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -20,7 +26,7 @@ public class Card_Encyclopedia {
     // values ATTACK, SKILL, POWER, STATUS, CURSE), cost (energy), baseBlock,
     // target (an enum with values ENEMY, ALL_ENEMY, SELF, NONE, SELF_AND_ENEMY,
     // ALL), color (enum with values RED, GREEN, BLUE, COLORLESS, CURSE)
-    public static List<AbstractCard> cards = new ArrayList<>();
+    public static List<AbstractCard> cards = new ArrayList();
     public static void update() {
         //red cards
         cards.add(new Bash());
@@ -99,6 +105,70 @@ public class Card_Encyclopedia {
         cards.add(new Offering());
 
         //colorless card
+        cards.add(new Apparition());
+        cards.add(new Bite());
+        cards.add(new JAX());
+        cards.add(new RitualDagger());
+        cards.add(new Shiv());
+        cards.add(new BandageUp());
+        cards.add(new Blind());
+        cards.add(new DarkShackles());
+        cards.add(new DeepBreath());
+        cards.add(new Discovery());
+        cards.add(new DramaticEntrance());
+        cards.add(new Enlightenment());
+        cards.add(new Finesse());
+        cards.add(new FlashOfSteel());
+        cards.add(new Forethought());
+        cards.add(new GoodInstincts());
+        cards.add(new Impatience());
+        cards.add(new JackOfAllTrades());
+        cards.add(new Madness());
+        cards.add(new MindBlast());
+        cards.add(new Panacea());
+        cards.add(new PanicButton());
+        cards.add(new Purity());
+        cards.add(new SwiftStrike());
+        cards.add(new Trip());
+        cards.add(new Apotheosis());
+        cards.add(new Chrysalis());
+        cards.add(new HandOfGreed());
+        cards.add(new Magnetism());
+        cards.add(new MasterOfStrategy());
+        cards.add(new Mayhem());
+        cards.add(new Metamorphosis());
+        cards.add(new Panache());
+        cards.add(new SadisticNature());
+        cards.add(new SecretTechnique());
+        cards.add(new SecretWeapon());
+        cards.add(new TheBomb());
+        cards.add(new ThinkingAhead());
+        cards.add(new Transmutation());
+        cards.add(new Violence());
+        cards.add(new Violence());
+
+        //curses
+        cards.add(new AscendersBane());
+        cards.add(new Clumsy());
+        cards.add(new Decay());
+        cards.add(new Doubt());
+        cards.add(new Injury());
+        cards.add(new Necronomicurse());
+        cards.add(new Normality());
+        cards.add(new Pain());
+        cards.add(new Parasite());
+        cards.add(new Pride());
+        cards.add(new Regret());
+        cards.add(new Shame());
+        cards.add(new Writhe());
+
+        //statuses
+        cards.add(new Burn());
+        cards.add(new Dazed());
+        cards.add(new Wound());
+        cards.add(new Slimed());
+        cards.add(new Void());
+
     }
     static {
         update();
